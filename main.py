ddb3281701352ec5176b7fe39e1e3d4c13365d19
vadimgggg <vadimbylovus@gmail.com>
2022-11-01 22:12:33 +0200

    upd

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import filedialog as fd
 import eel
 
 # initialyze program
@@ -12,5 +13,9 @@ def get_proba():
 def send_proba(msg):
     print(msg)
 
+def open_file_dialog():
+    filename = fd.askopenfilename()
+    print(filename)
+
 # main window
-eel.start('index.html')
+eel.start("index.html", size=(800, 600))
