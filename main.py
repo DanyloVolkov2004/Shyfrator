4850f5a570f6c8a573e96432e61a9c3d3b3f9d13
vadimgggg <vadimbylovus@gmail.com>
2022-11-03 22:30:30 +0200

    upd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import filedialog as fd
+import crypto.crypto as crypto
 import eel
 
 # initialyze program
@@ -17,6 +18,16 @@ def send_proba(msg):
 def open_file_dialog():
     filename = fd.askopenfilename()
     print(filename)
+
+@eel.expose
+def encode(method:str, password:str, source_file_path:str, destination_file_path:str):
+    cr = crypto.Crypto(method, password)
+    cr.encrypt_file(source_file_path, destination_file_path)
+
+@eel.expose
+def decode(method:str, password:str, source_file_path:str, destination_file_path:str):
+    cr = crypto.Crypto(method, password)
+    cr.encrypt_file(source_file_path, destination_file_path)
 
 # main window
 eel.start("index.html", size=(800, 600))
