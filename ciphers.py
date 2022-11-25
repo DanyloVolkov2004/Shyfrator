edc52363b9fb32825889f54ca16d6192b5649490
IvanKoshan2004 <ivan.koshangod@gmail.com>
2022-11-26 01:58:01 +0200

    removed the cipher detection code

--- ciphers.py
+++ ciphers.py
@@ -3,24 +3,20 @@ ciphers = {
         "key_length": 32,
         "type": "block",
         "block_size": 16,
-        "header_byte": b"\xae"
     },
     "Blowfish": {
         "key_length": 24,
         "type": "block",
         "block_size": 8,
-        "header_byte": b"\xbf"
     },
     "Camellia": {
         "key_length": 16,
         "type": "block",
         "block_size": 16,
-        "header_byte": b"\xca"
     },
     "ChaCha20": {
         "key_length": 32,
         "type": "stream",
         "nonce_size": 16,
-        "header_byte": b"\xcc"
     }
 }
